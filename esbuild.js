e60cd9979c7b15c3d2e69c559a12c49d3228df87
François Wouts <f@zenc.io>
2022-02-24 22:26:15 +1100

    add potential alternative

--- esbuild.js
+++ esbuild.js
@@ -9,6 +9,10 @@ build({
   platform: "node",
   define: {
     "process.env.FOO": JSON.stringify("Hello, World!"),
+    // The following fails with:
+    // error: The define key "process.env[\"FOO\"]" contains invalid identifier "env[\"FOO\"]"
+    //
+    // 'process.env["FOO"]': JSON.stringify("Hello, World!"),
   },
 }).catch((err) => {
   console.error(err);
